15cb2f9dac5198102fb095528f0c1ed90326bfb8
Hadi-Husein <hadi.husein.te16C@hotmail.com>
2019-01-29 15:11:58 +0100

    Intro code

---
+++ src/Car.java
@@ -0,0 +1,35 @@
+import javafx.scene.paint.Color;
+
+public abstract class Car {
+	public double enginePower; // Engine power of the car
+	public double currentSpeed; // The current speed of the car
+	public Color color; // Color of the car
+	public String modelName; // The car model name
+
+	public Volvo740() {
+		color = Color.RED;
+		enginePower = 100;
+		modelName = "Volvo740";
+		stopEngine();
+	}
+
+	public double getEnginePower() {
+		return enginePower;
+	}
+
+	public double getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void startEngine() {
+		currentSpeed = 0.1;
+	}
+
+	public void stopEngine() {
+		currentSpeed = 0;
+	}
+}
