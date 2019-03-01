69e9fd93ed053d42d2a9a5c7fa31c1fbac5eb0f5
Hadi-Husein <hadi.husein.te16C@hotmail.com>
2019-03-01 12:42:45 +0100

    cosomak

---
+++ src/carTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class carTest {
+	
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+	
+}
